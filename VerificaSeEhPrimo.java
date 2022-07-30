74722bbe2a02375246df350afd23a39670aab06e
Yuri Gabriel <73195850+yuriCovalski@users.noreply.github.com>
2022-07-29 21:43:11 -0300

    Add files via upload

---
+++ VerificaSeEhPrimo.java
@@ -0,0 +1,15 @@
+
+public class VerificaSeEhPrimo {
+	
+	public static int a(int num) {
+		if (num %2 == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
