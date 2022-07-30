316458774d1f245773ed72b3077d04aad1320769
Yuri Gabriel <73195850+yuriCovalski@users.noreply.github.com>
2022-07-29 21:46:17 -0300

    descrevendo o problema

--- VerificaSeEhPrimo.java
+++ VerificaSeEhPrimo.java
@@ -1,6 +1,16 @@
 
 public class VerificaSeEhPrimo {
 	
+	/*Descrição do problema:
+	Crie uma função que retorna se o número é primo. Caso seja primo, retorne 1, caso contrário retorne 0.
+	
+	*/
+	
+	/*Meus comentários sobre o problema:
+	Problema para iniciantes no java.
+	
+	*/
+	
 	public static int a(int num) {
 		if (num %2 == 0) {
 			return 1;
